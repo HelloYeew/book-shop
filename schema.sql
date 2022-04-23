61b458cf4555079a15f76839844865131d740f9b
Phawit Pornwattanakul <me@helloyeew.dev>
2022-04-24 03:44:42 +0700

    Add SQL schema for create the table

---
+++ schema.sql
@@ -0,0 +1,26 @@
+-- Schema for create the novel shop database using the SQLite data type
+-- To create the database, run the following command:
+-- sqlite3 novelshop.db < schema.sql
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    subgenre TEXT NOT NULL,
+    height INTEGER NOT NULL,
+    publisher TEXT,
+    price REAL NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS history (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    buyer_id INTEGER NOT NULL,
+    book_id INTEGER NOT NULL,
+    FOREIGN KEY (buyer_id) REFERENCES users(id)
+);
